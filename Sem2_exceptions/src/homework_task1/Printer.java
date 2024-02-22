475348690521160b34bc361ae15693bbb4736ac1
antonbashlovka <anton.bashlovka@gmail.com>
2024-02-22 10:46:26 +0500

    done task1

---
+++ Sem2_exceptions/src/homework_task1/Printer.java
@@ -0,0 +1,18 @@
+package homework_task1;
+
+public class Printer {
+    public static void main(String[] args) {
+        String input;
+
+// При отправке кода на Выполнение, вы можете варьировать эти параметры
+        if (args.length == 0) {
+            input = "3.14"; // По умолчанию используем "3.14", если аргумент не передан
+        } 
+        else {
+            input = args[0];
+        }
+
+        float result = IsFloat.isFloat(input);
+        System.out.println(result);
+    }
+}
